3aba86f60f22d0a607475d1fa24dd63a5f28df21
Parminder Singh <parminder.singh@mavenwave.com>
2023-02-07 01:48:57 +0530

    images updated

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  { useState, useEffect } from "react";
 import "./Greeting.css";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
@@ -7,8 +7,43 @@ import { Fade } from "react-reveal";
 import FeelingProud from "./FeelingProud";
 import { auto } from "@popperjs/core";
 
+function GreetingImage() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    `${process.env.PUBLIC_URL}/assests/images/gamer_traveller_fun_lover.png`,
+    `${process.env.PUBLIC_URL}/assests/images/guy_playing_video_games.png`,
+    `${process.env.PUBLIC_URL}/assests/images/gamer_traveller_fun_lover_1.png`,
+    `${process.env.PUBLIC_URL}/assests/images/gamer_traveller_fun_lover_2.png`
+  ];
+
+  function getCurrentImage() {
+    return currentImage === 1 ? currentImage + 1 : currentImage;
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((getCurrentImage() + 1) % images.length);
+    }, 5000); // 5s
+    return () => clearInterval(intervalId);
+  }, [getCurrentImage(), images.length]);
+
+  return (
+    <div className="greeting-image-div">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={`greeting-image ${index === getCurrentImage() ? 'current' : ''}`}
+          src={image}
+          alt="greeting"
+        />
+      ))}
+    </div>
+  );
+}
+
 export default function Greeting(props) {
   const theme = props.theme;
+  
   return (
     <Fade bottom duration={2000} distance="40px">
       <div className="greet-main" id="greeting">
@@ -49,7 +84,8 @@ export default function Greeting(props) {
 							src={require("../../assests/images/feelingProud.svg")}
 						></img> */}
             {/* <FeelingProud theme={theme} /> */}
-            <img src={`${process.env.PUBLIC_URL}/assests/images/guy_playing_video_games.png`} alt='guy_playing_video_games.png'/>
+            {/* <img src={`${process.env.PUBLIC_URL}/assests/images/gamer_traveller_fun_lover.png`} alt='gamer_traveller_fun_lover.png'/> */}
+            <GreetingImage theme={theme} />
           </div>
         </div>
       </div>
